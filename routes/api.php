abadef45744daa4e007a09aeac5ac4bb1e9e8e40
vicky <pratamavr@gmail.com>
2026-02-04 04:15:54 +0800

    updates

--- routes/api.php
+++ routes/api.php
@@ -2,3 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Telegram\Http\Controllers\TelegramController;
+use Modules\Telegram\Http\Controllers\TelegramWebhookController;
+
+Route::prefix("telegram")->group(function () {
+	Route::post("webhook", [TelegramWebhookController::class, "handleWebhook"])
+		->withoutMiddleware(["auth:sanctum", "auth"])
+		->name("telegram.webhook");
+});
