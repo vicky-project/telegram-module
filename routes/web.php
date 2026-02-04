9fc17cabe74657763e501d3fc80b484e51d4ee9b
vicky <pratamavr@gmail.com>
2026-02-04 12:03:11 +0800

    updates

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,6 @@ Route::prefix("telegram")
 	->group(function () {
 		Route::middleware(["auth"])->group(function () {
 			Route::get("", [TelegramController::class, "index"]);
-			Route::post("generate-code", [
-				TelegramLinkController::class,
-				"generateCode",
-			])->name("generate-code");
-			Route::post("unlink", [TelegramLinkController::class, "unlink"])->name(
-				"unlink"
-			);
-			Route::post("update-settings", [
-				TelegramLinkController::class,
-				"updateSettings",
-			])->name("update-settings");
 		});
 
 		Route::get("redirect", [TelegramController::class, "redirect"])->name(
