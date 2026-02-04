df17058dac2f33fc392d1b2a8e8468d2ce379d56
vicky <pratamavr@gmail.com>
2026-02-04 22:11:43 +0800

    updates

--- resources/views/partials/telegram_info.blade.php
+++ resources/views/partials/telegram_info.blade.php
@@ -1,7 +1,8 @@
+<h1 class="card-title mb-4 pb-2 border-bottom border-success">Telegram</h1>
 @if($user->hasTelegram())
-<div class="row">
-  <div class="col-md-6 mb-3">
-    <div class="border border-primary border-opacity-50 rounded-circle">
+<div class="row align-items-center">
+  <div class="col-md-6 mb-3 text-center">
+    <div class="border border-primary border-opacity-50 rounded-circle text-center">
       <i class="
       @switch(config('telegram.injection.icon-provider', 'fontawesome'))
         @case('fontawesome')
@@ -13,8 +14,15 @@
       @endswitch
       display-1"></i>
     </div>
+    <h2 class="fw-bold">Connected</h2>
   </div>
-  <div class="col-md-6 mb-3"></div>
+  <div class="col-md-6 mb-3">
+     <p class="mb-0">
+        <span class="text-muted">Akun:</span>
+        <strong>{{ $user->telegram->username ?? $user->telegram->first_name.' '.$user->telegram->last_name }}</strong>
+      </p>
+      <small class="text-muted">Chat ID: {{ $user->telegram->telegram_id }}</small>
+  </div>
 </div>
 @else
   <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="{{ config('telegram.bot.username') }}" data-size="{{ config('telegram.widgets.size') }}" data-auth-url="{{ config('telegram.widgets.redirect_url') }}" data-request-access="write"
