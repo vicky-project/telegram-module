860b5cb3eec376ff32bff11eb7cca477048d9b35
vicky <pratamavr@gmail.com>
2026-02-06 15:04:18 +0800

    updates

--- app/Services/TelegramService.php
+++ app/Services/TelegramService.php
@@ -97,7 +97,7 @@ class TelegramService
 
 	public function getUserByChatId(int $chatId)
 	{
-		return $this->telegram->byTelegramId($chatId)->provider->user;
+		return $this->telegram->getByChatId($chatId)->provider->user;
 	}
 
 	public function unlink(User $user, int $telegramId): bool
