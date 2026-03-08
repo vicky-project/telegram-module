84213348f1235de9da8acb185be79c8300bc85f4
vicky <pratamavr@gmail.com>
2026-03-09 00:06:01 +0800

    updates

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,54 +2,27 @@
 
 namespace Modules\Telegram\Providers;
 
-<<<<<<< HEAD
-use Modules\Telegram\Listeners\LinkTelegramOnLogin;
-use Illuminate\Auth\Events\Login;
-=======
->>>>>>> 984b245 (updates)
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
-<<<<<<< HEAD
-	/**
-	 * The event handler mappings for the application.
-	 *
-	 * @var array<string, array<int, string>>
-	 */
-	protected $listen = [Login::class => [LinkTelegramOnLogin::class]];
+  /**
+  * The event handler mappings for the application.
+  *
+  * @var array<string, array<int, string>>
+  */
+  protected $listen = [];
 
-	/**
-	 * Indicates if events should be discovered.
-	 *
-	 * @var bool
-	 */
-	protected static $shouldDiscoverEvents = true;
+  /**
+  * Indicates if events should be discovered.
+  *
+  * @var bool
+  */
+  protected static $shouldDiscoverEvents = true;
 
-	/**
-	 * Configure the proper event listeners for email verification.
-	 */
-	protected function configureEmailVerification(): void
-	{
-	}
-=======
-    /**
-     * The event handler mappings for the application.
-     *
-     * @var array<string, array<int, string>>
-     */
-    protected $listen = [];
+  /**
+  * Configure the proper event listeners for email verification.
+  */
+  protected function configureEmailVerification(): void {}
 
-    /**
-     * Indicates if events should be discovered.
-     *
-     * @var bool
-     */
-    protected static $shouldDiscoverEvents = true;
-
-    /**
-     * Configure the proper event listeners for email verification.
-     */
-    protected function configureEmailVerification(): void {}
->>>>>>> 984b245 (updates)
 }
--- config/config.php
+++ config/config.php
@@ -1,52 +1,6 @@
 <?php
 
 return [
-<<<<<<< HEAD
-	"name" => "Telegram",
-	"backto_server_url" => "",
-
-	"bot" => [
-		"token" => env("TELEGRAM_BOT_TOKEN"),
-		"username" => env("TELEGRAM_BOT_USERNAME"),
-		"webhook_url" => env("TELEGRAM_WEBHOOK_URL", "/api/telegram/webhook"),
-		"webhook_secret" => env("TELEGRAM_WEBHOOK_SECRET"),
-		"admin" => env("TELEGRAM_ADMINS", ""), // String of id with comma separated
-		"allowed_updates" => ["message", "callback_query", "edited_message"],
-		"cache" => [
-			"prefix" => env("TELEGRAM_BOT_CACHE_PREFIX", "telegram_reply:"),
-			"duration" => env("TELEGRAM_BOT_CACHE_DURATION", 60), // in minutes
-		],
-	],
-
-	"widgets" => [
-		"size" => "large",
-		"userpic" => false,
-		// for user auth in setting profile page
-		"redirect_url_auth" => env(
-			"TELEGRAM_AUTH_REDIRECT_URL",
-			url("/telegram/redirect-auth")
-		),
-
-		// for form login
-		"redirect_url_login" => env(
-			"TELEGRAM_LOGIN_REDIRECT_URL",
-			url("/telegram/redirect-login")
-		),
-	],
-
-	"commander" => [
-		// For EnsureUserLoginMiddleware to except from checking
-		"no_auth" => ["start", "help", "link"],
-	],
-
-	// Inject telegram connect button and detail to User Management Module profile via hook core's.
-	"hooks" => [
-		"enabled" => true,
-		"service" => \Modules\Core\Services\HookService::class,
-		"icon-provider" => "fontawesome", // fontawesome, bootstrap-icon
-	],
-];
-=======
   'name' => 'Telegram',
   'base_url' => env('APP_URL').'/telegram',
   "logo_url" => env("LOGO_URL", "/homeserver.png"),
@@ -70,4 +24,3 @@ return [
     "service" => \Modules\CoreUI\Services\UIService::class,
   ],
 ];
->>>>>>> 984b245 (updates)
