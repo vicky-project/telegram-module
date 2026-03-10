e55425c2565f8d7c16d43689e053504523739894
vicky <pratamavr@gmail.com>
2026-03-10 19:08:26 +0800

    updates

--- app/Http/Middleware/AuthenticateWithTelegram.php
+++ app/Http/Middleware/AuthenticateWithTelegram.php
@@ -15,7 +15,7 @@ class AuthenticateWithTelegram
 
     $telegramUser = session('telegram_user');
     if (!$telegramUser) {
-      \Log::error("Data user telegram tidak ditemukan.");
+      \Log::error("Data user telegram tidak ditemukan.", session()->all());
       return redirect()->route('telegram.entry')->with('error', 'Data Telegram tidak ditemukan.');
     }
 
