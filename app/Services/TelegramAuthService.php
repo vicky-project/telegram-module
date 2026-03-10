af58c2c24ccb417da4c8e824bb41999a02f64b5b
vicky <pratamavr@gmail.com>
2026-03-11 00:25:38 +0800

    updates

--- app/Services/TelegramAuthService.php
+++ app/Services/TelegramAuthService.php
@@ -20,6 +20,7 @@ class TelegramAuthService
     bool $isWebApp = true
   ): bool {
     parse_str($initData, $params);
+    \Log::debug("Parsing init data: ", $params);
 
     $hash = $params["hash"] ?? null;
     if (!$hash) return false;
