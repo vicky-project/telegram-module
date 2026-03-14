6518ff36e2712a0870f8f81beac5f19ae39a2434
vicky <pratamavr@gmail.com>
2026-03-14 19:46:39 +0800

    updates

--- app/Http/Controllers/MiniApp/MiniAppController.php
+++ app/Http/Controllers/MiniApp/MiniAppController.php
@@ -7,9 +7,7 @@ use Illuminate\Http\Request;
 class MiniAppController extends Controller
 {
   public function index(Request $request) {
-    dd($request->all());
-    $token = $request->get("token");
-    return view('telegram::index', compact("token"));
+    return view('telegram::index');
   }
 
   public function profile() {
