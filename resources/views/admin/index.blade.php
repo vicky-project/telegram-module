9216a6bf1fca5e867a231dc387a204ab8613f91b
vicky <pratamavr@gmail.com>
2026-03-28 00:09:05 +0800

    updates

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -24,7 +24,7 @@
         </p>
         @endif
         @php
-        $authDate = isset($user->data["auth_date"]) ? Carbon::parse($user->data["auth_date"]) : 'Never';
+        $authDate = isset($user->data["auth_date"]) ? Carbon::parse($user->data["auth_date"])->diffForHumans() : 'Never';
         @endphp
         <span class="small text-muted">Last Used:</span> {{ $authDate }}
       </div>
