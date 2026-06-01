b46708aaafd212e912a92da8d20ac1a101ef9ce9
vicky <pratamavr@gmail.com>
2026-06-01 08:55:28 +0800

    updates

--- app/Services/TelegramService.php
+++ app/Services/TelegramService.php
@@ -154,9 +154,11 @@ class TelegramService
   public static function findTelegramId($userId): ?int
   {
     if (!$userId || $userId === null) {
+      \Log::error("User id null", ['user_id' => $userId]);
       return null;
     }
     if (!class_exists(SocialAccountService::class)) {
+      \Log::error("Social Account not installed.", ['social' => SocialAccountService::class]);
       return null;
     }
 
@@ -164,15 +166,18 @@ class TelegramService
     $socialAccounts = $service->getByUserId($userId);
 
     if (!$socialAccounts || $socialAccounts->isEmpty()) {
+      \Log::error('Social account empty.', ['social' => $socialAccounts]);
       return null;
     }
 
     $telegram = $socialAccounts->where('provider', Provider::TELEGRAM)->first();
 
     if (!$telegram || !$telegram->providerable) {
+      \Log::error('Telegram tidak ada.', ['telegram' => $telegram]);
       return null;
     }
 
+    \Log::debug('result', ['telegram' => $telegram]);
     return $telegram->telegram_id;
   }
 }
